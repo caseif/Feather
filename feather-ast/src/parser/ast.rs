eecbc2ce54f1dff7f7130aa60fd5878d1dd586d5
Max Roncace <me@caseif.net>
2024-03-17 18:58:30 -0400

    Add initial impl to transform parse tree to AST

--- feather-ast/src/parser/ast.rs
+++ feather-ast/src/parser/ast.rs
@@ -1,11 +1,12 @@
 use featherparse::{Cst, CstNode, TokenList};
 use serde::Serialize;
 use std::collections::{HashMap, VecDeque};
+use lazy_static::lazy_static;
 
 use crate::parser::ParseError;
 use crate::parser::cst::generate_cst;
 
-#[derive(Debug, Serialize)]
+#[derive(Clone, Debug, Serialize)]
 pub enum AstNodeType {
     Program,
     Block,
@@ -23,9 +24,9 @@ pub enum AstNodeType {
     Assignment,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Clone, Debug, Serialize)]
 pub struct AstNode {
-    node_type: AstNodeType,
+    node_type: String,
     val: Option<String>,
     children: Vec<AstNode>,
 }
@@ -33,6 +34,131 @@ pub struct AstNode {
 #[derive(Debug, Serialize)]
 pub struct Ast {
     root: AstNode,
+}
+
+enum UniversalRule {
+    Remove,
+    Flatten,
+    PullUpChildValue,
+    // flattens if only 1 child, otherwise renames based on nth child and removes it
+    PullUpOperator,
+}
+
+lazy_static! {
+    static ref EXPR_RULES: HashMap<&'static str, UniversalRule> = HashMap::from([
+        ("IntType", UniversalRule::Flatten),
+        ("UintType", UniversalRule::Flatten),
+        ("FloatType", UniversalRule::Flatten),
+        ("BuiltInType", UniversalRule::Flatten),
+        ("Type", UniversalRule::Flatten),
+        ("Literal", UniversalRule::Flatten),
+        ("ArrayModNaught", UniversalRule::Flatten),
+        ("TupleTypes", UniversalRule::Flatten),
+        ("TupleElements", UniversalRule::Flatten),
+        ("TupleElsPrime", UniversalRule::Flatten),
+        ("InitListEntries", UniversalRule::Flatten),
+        ("Invokable", UniversalRule::Flatten),
+        ("FnParamListPrime", UniversalRule::Flatten),
+        ("IfChainPrime", UniversalRule::Flatten),
+        ("FnPmDefsPrime", UniversalRule::Flatten),
+        ("ClassFields", UniversalRule::Flatten),
+        ("ClassFldsPrime", UniversalRule::Flatten),
+        ("ClassFunctions", UniversalRule::Flatten),
+        ("ClassFnsPrime", UniversalRule::Flatten),
+        ("Annotatable", UniversalRule::Flatten),
+        ("StatementNaught", UniversalRule::Flatten),
+        ("StatementList", UniversalRule::Flatten),
+        ("Primary", UniversalRule::Flatten),
+        ("OpUnary", UniversalRule::Flatten),
+        ("OpNumMult", UniversalRule::Flatten),
+        ("OpNumAdd", UniversalRule::Flatten),
+        ("CmpBoolRel", UniversalRule::Flatten),
+        ("CmpBoolEq", UniversalRule::Flatten),
+        ("OpBoolAnd", UniversalRule::Flatten),
+        ("OpBoolOr", UniversalRule::Flatten),
+        ("Statement", UniversalRule::Flatten),
+        ("ComplexType", UniversalRule::PullUpChildValue),
+        ("ExprOpUnary", UniversalRule::PullUpOperator),
+        ("ExprOpNumMult", UniversalRule::PullUpOperator),
+        ("ExprOpNumAdd", UniversalRule::PullUpOperator),
+        ("ExprCmpBoolRel", UniversalRule::PullUpOperator),
+        ("ExprCmpBoolEq", UniversalRule::PullUpOperator),
+        ("ExprOpBoolAnd", UniversalRule::PullUpOperator),
+        ("ExprOpBoolOr", UniversalRule::PullUpOperator),
+        ("OptNewline", UniversalRule::Remove),
+        ("RangeDelimiter", UniversalRule::Remove),
+        ("StatementEnd", UniversalRule::Remove),
+    ]);
+
+    // Rules for operator expressions specifically. The value is the index of
+    // the child to operate on alongside mappings from original child names to
+    // new parent names.
+    static ref OPERATOR_RULES: HashMap<&'static str, (usize, HashMap<&'static str, &'static str>)> = HashMap::from([
+        ("ExprOpUnary", (0, HashMap::from([
+            ("Not", "OpNot"), // invert boolean value
+            ("Hyphen", "OpNegate"), // negate numeric value
+        ]))),
+        ("ExprOpNumMult", (1, HashMap::from([
+            ("Asterisk", "OpMultiply"),
+            ("ForwardSlash", "OpDivide"),
+        ]))),
+        ("ExprOpNumAdd", (1, HashMap::from([
+            ("Plus", "OpAdd"),
+            ("Hyphen", "OpSubtract"),
+            ("Percent", "OpModulo"),
+        ]))),
+        ("ExprCmpBoolRel", (1, HashMap::from([
+            ("LessThan", "CmpLess"),
+            ("LessEqual", "CmpLessEq"),
+            ("GreaterThan", "CmpGreater"),
+            ("GreaterEqual", "CmpGreaterEq"),
+        ]))),
+        ("ExprCmpBoolEq", (1, HashMap::from([
+            ("Equals", "CmpEquals"),
+            ("NotEquals", "CmpNotEquals"),
+        ]))),
+        ("ExprOpBoolAnd", (1, HashMap::from([
+            ("And", "OpAnd"),
+        ]))),
+        ("ExprOpBoolOr", (1, HashMap::from([
+            ("Or", "OpOr"),
+        ]))),
+    ]);
+
+    static ref PRESERVE_TOKENS: Vec<&'static str> = Vec::from([
+        "TypeInt8",
+        "TypeInt16",
+        "TypeInt32",
+        "TypeInt64",
+        "TypeUnt8",
+        "TypeUnt16",
+        "TypeUnt32",
+        "TypeUnt64",
+        "TypeFloat8",
+        "TypeFloat16",
+        "TypeFloat32",
+        "TypeFloat64",
+        "TypeBool",
+        "TypeChar",
+        "TypeString",
+        "Nil",
+        "Range",
+        "RangeInclusive",
+        "Not",
+        "Plus",
+        "Hyphen",
+        "Percent",
+        "Asterisk",
+        "ForwardSlash",
+        "LessThan",
+        "LessEqual",
+        "GreaterThan",
+        "GreaterEqual",
+        "Equals",
+        "NotEquals",
+        "And",
+        "Or",
+    ]);
 }
 
 type TreePath = Vec<usize>;
@@ -68,13 +194,67 @@ fn flatten_cst<'a>(cst: &'a Cst) -> Vec<(&'a CstNode, TreePath)> {
     return flat_cst;
 }
 
-fn process_cst_node(cst_node: &CstNode, children: Vec<AstNode>) -> Option<AstNode> {
-    if let CstNode::Expression(expr) = cst_node {
-        if expr.type_id == "Program" {
-            return Some(AstNode { node_type: AstNodeType::Program, val: None, children });
+fn process_cst_node(cst_node: &CstNode, children: Vec<AstNode>) -> Vec<AstNode> {
+    match cst_node {
+        CstNode::Expression(expr) => {
+            if let Some(rule) = EXPR_RULES.get(expr.type_id.as_str()) {
+                match rule {
+                    UniversalRule::Remove => {
+                        return vec![];
+                    }
+                    UniversalRule::Flatten => {
+                        return children;
+                    }
+                    UniversalRule::PullUpChildValue => {
+                        if children.len() != 1 {
+                            panic!("Expression must have exactly 1 child to pull value up from");
+                        }
+
+                        return vec![AstNode {
+                            node_type: expr.type_id.clone(),
+                            val: children[0].val.clone(),
+                            children: vec![]
+                        }];
+                    }
+                    UniversalRule::PullUpOperator => {
+                        if children.len() == 1 {
+                            return children;
+                        } else {
+                            let (child_index, mappings) = OPERATOR_RULES.get(expr.type_id.as_str())
+                                    .expect("Operator expression should have corresponding operator rule");
+                            if *child_index >= children.len() {
+                                panic!("Operator expression does not have enough children");
+                            }
+                            let op_token = &children[*child_index].node_type;
+                            let new_name = mappings.get(op_token.as_str())
+                                    .expect("Operator token should have corresponding name mapping");
+                            return vec![AstNode {
+                                node_type: new_name.to_string(),
+                                val: None,
+                                children: children.into_iter().take(*child_index).skip(1).collect() }];
+                        }
+                    }
+                }
+            }
+
+            return vec![AstNode {
+                node_type: expr.type_id.clone(),
+                val: None,
+                children
+            }];
+        }
+        CstNode::Token(token) => {
+            if token.value.is_some() || PRESERVE_TOKENS.contains(&token.type_id.as_str()) {
+                return vec![AstNode {
+                    node_type: token.type_id.clone(),
+                    val: token.value.clone(),
+                    children: vec![]
+                }];
+            } else {
+                return vec![];
+            }
         }
     }
-    return None;
 }
 
 fn convert_to_ast(cst: &Cst) -> Result<Ast, ParseError> {
@@ -85,14 +265,14 @@ fn convert_to_ast(cst: &Cst) -> Result<Ast, ParseError> {
     for (node, path) in &flat_cst {
         let processed_children = child_map.remove(path).unwrap_or(vec![]);
 
-        if let Some(ast_node) = process_cst_node(node, processed_children) {
-            if path.len() == 0 {
-                return Ok(Ast { root: ast_node });
-            }
+        let mut new_nodes = process_cst_node(node, processed_children);
 
-            let parent_path = path[0..(path.len() - 1)].into_iter().cloned().collect();
-            child_map.entry(parent_path).or_insert(vec![]).push(ast_node);
+        if path.len() == 0 {
+            return Ok(Ast { root: new_nodes[0].clone() });
         }
+
+        let parent_path = path[0..(path.len() - 1)].into_iter().cloned().collect();
+        child_map.entry(parent_path).or_insert(vec![]).append(&mut new_nodes);
     }
 
     panic!("Flat tree did not contain root node!");
